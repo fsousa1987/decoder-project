24259be8a8d2786d2db1e0dda710dd6ed9d06b17
francisco <franciscoeds1987@gmail.com>
2022-06-27 09:24:57 -0300

    Implementar retry Resilience4j e configurar timeout

--- ead/authuser/src/main/java/com/ead/authuser/clients/CourseClient.java
+++ ead/authuser/src/main/java/com/ead/authuser/clients/CourseClient.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -31,6 +33,7 @@ public class CourseClient {
     @Value("${ead.api.url.course}")
     String REQUEST_URL_COURSE;
 
+    //    @Retry(name = "retryInstance", fallbackMethod = "retryfallback")
     public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable) {
         List<CourseDto> searchResult = null;
         ResponseEntity<ResponsePageDto<CourseDto>> result = null;
@@ -38,6 +41,7 @@ public class CourseClient {
 
         log.debug("Request URL: {}", url);
         log.info("Request URL: {}", url);
+        System.out.println("---Start Request ao Course Microservice---");
         try {
             ParameterizedTypeReference<ResponsePageDto<CourseDto>> responseType =
                     new ParameterizedTypeReference<ResponsePageDto<CourseDto>>() {
@@ -51,5 +55,11 @@ public class CourseClient {
         }
         log.info("Ending request /courses userId {}", userId);
         return result.getBody();
+    }
+
+    public Page<CourseDto> retryfallback(UUID userId, Pageable pageable, Throwable t) {
+        log.error("Inside retry retryfallback, cause - {}", t.toString());
+        List<CourseDto> searchResult = new ArrayList<>();
+        return new PageImpl<>(searchResult);
     }
 }
